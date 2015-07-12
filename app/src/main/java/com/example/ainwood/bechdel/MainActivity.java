a2b7d886d4e60445fe7e0d03fade8bff0ae6c820
Andrew Inwood <ainwood87@gmail.com>
2015-07-12 02:04:28 -0400

    Create fragment for main activity
    Support orientation change by creating fragment

--- app/src/main/java/com/example/ainwood/bechdel/MainActivity.java
+++ app/src/main/java/com/example/ainwood/bechdel/MainActivity.java
@@ -7,84 +7,36 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-
-import java.net.URLEncoder;
-import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
-    private static final String queryString = "http://bechdeltest.com/api/v1/getMoviesByTitle?title=";
-    private MovieViewAdapter adapter;
-    RecyclerView recList;
-    private QueryThread workThread;
-    boolean requestKill = false;
+
     private final Activity activity = this;
+    private SearchFragment searchFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layouut.activity_main);
-        handleIntent(getIntent());
-        LayoutInflater inflater = (LayoutInflater) getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.activity_main, null);
-        recList = (RecyclerView) view.findViewById(R.id.cardList);
-        LinearLayoutManager llm = new LinearLayoutManager(this);
-        llm.setOrientation(LinearLayoutManager.VERTICAL);
-        recList.setLayoutManager(llm);
-
-        adapter = new MovieViewAdapter();
-        recList.setAdapter(adapter);
-        setContentView(view);
+//        handleIntent(getIntent());
+        searchFragment = (SearchFragment) getFragmentManager().findFragmentById(android.R.id.content);
+        if (searchFragment == null) {
+            searchFragment = new SearchFragment();
+            getFragmentManager().beginTransaction()
+                    .add(android.R.id.content,
+                            searchFragment).commit();
+        }
     }
 
     @Override
     protected void onNewIntent(Intent intent)
     {
-        handleIntent(intent);
+        searchFragment.handleIntent(intent);
     }
 
-    private void handleIntent(Intent intent) {
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            //use the query to search your data somehow
-            String str = "";
-            try {
-                str = URLEncoder.encode(query, "utf-8");
-            } catch (Exception e) {
-
-            }
-            adapter.clearAllData();
-            final String bechdelString = queryString + str;
-            ArrayList<MovieInfo> movieInfoArrayList = new ArrayList<MovieInfo>();
-            if (workThread == null) {
-                workThread = new QueryThread(this, adapter, bechdelString);
-                workThread.start();
-            } else {
-                workThread.setRequestKill(true);
-                final Handler myHandler = new Handler();
-
-                myHandler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (workThread.isAlive()) {
-                            myHandler.postDelayed(this, 100);
-                        } else {
-                            workThread = new QueryThread(activity, adapter, bechdelString);
-                            workThread.start();
-                        }
-                    }
-                }, 100);
-            }
-        }
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ app/src/main/java/com/example/ainwood/bechdel/SearchFragment.java
@@ -0,0 +1,80 @@
+package com.example.ainwood.bechdel;
+
+import android.app.Fragment;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
+/**
+ * Created by ainwo_000 on 7/12/2015.
+ */
+public class SearchFragment extends Fragment {
+    private static final String queryString = "http://bechdeltest.com/api/v1/getMoviesByTitle?title=";
+    private MovieViewAdapter adapter;
+    RecyclerView recList;
+    private QueryThread workThread;
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_main, null);
+        recList = (RecyclerView) view.findViewById(R.id.cardList);
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recList.setLayoutManager(llm);
+        if (adapter == null) {
+            adapter = new MovieViewAdapter();
+        }
+        recList.setAdapter(adapter);
+        return view;
+    }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    public void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //use the query to search your data somehow
+            String str = "";
+            try {
+                str = URLEncoder.encode(query, "utf-8");
+            } catch (Exception e) {
+
+            }
+            adapter.clearAllData();
+            final String bechdelString = queryString + str;
+            ArrayList<MovieInfo> movieInfoArrayList = new ArrayList<MovieInfo>();
+            if (workThread == null) {
+                workThread = new QueryThread(getActivity(), adapter, bechdelString);
+                workThread.start();
+            } else {
+                workThread.setRequestKill(true);
+                final Handler myHandler = new Handler();
+
+                myHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (workThread.isAlive()) {
+                            myHandler.postDelayed(this, 100);
+                        } else {
+                            workThread = new QueryThread(getActivity(), adapter, bechdelString);
+                            workThread.start();
+                        }
+                    }
+                }, 100);
+            }
+        }
+    }
+}
